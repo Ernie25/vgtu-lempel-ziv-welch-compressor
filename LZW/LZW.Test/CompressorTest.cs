b1eaf9cc09a8864bd7430b4e1dd41c9b10706b74
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-04-17 20:51:12 +0300

    Add Compressor Encoding functionality with initial files.

---
+++ LZW/LZW.Test/CompressorTest.cs
@@ -0,0 +1,164 @@
+using Xunit;
+using LZW.LZW;
+using System.Collections.Generic;
+
+namespace LZW.Test
+{
+    public class CompressorTest
+    {
+        [Fact]
+        public void Test_IsDictionaryFilled_DictionaryContainsElements()
+        {
+            // Arrange
+            short asciiLength = 256;
+            Compressor Compressor = new();
+
+            // Act
+            int numberOfElements = Compressor.Dictionary.Count;
+
+            // Assert
+            Assert.NotEqual(0, numberOfElements);
+            Assert.Equal(asciiLength, numberOfElements);
+        }
+
+        [Theory]
+        [MemberData(nameof(EncodingData))]
+        public void Test_Encode_StringSuccessfullyEncoded(string text, List<int> expectedEncoding)
+        {
+            // Arrange
+            Compressor compressor = new();
+
+            // Act
+            var resultList = compressor.Encode(text);
+
+            // Assert
+            Assert.Equal(resultList.Count, expectedEncoding.Count);
+            for (int i = 0; i < resultList.Count; i++)
+            {
+                Assert.Equal(resultList[i], expectedEncoding[i]);
+            }
+        }
+
+        private static IEnumerable<object[]> EncodingData()
+        {
+            yield return new object[] {
+                "test",
+                new List<int>
+                {
+                    116,
+                    101,
+                    115,
+                    116
+                }
+            };
+            yield return new object[] {
+                "Lorem Ipsum",
+                new List<int>
+                {
+                    76,
+                    111,
+                    114,
+                    101,
+                    109,
+                    32,
+                    73,
+                    112,
+                    115,
+                    117,
+                    109
+                } };
+            yield return new object[] {
+                "Lempel Ziv Welch",
+                new List<int>
+                {
+                    76,
+                    101,
+                    109,
+                    112,
+                    101,
+                    108,
+                    32,
+                    90,
+                    105,
+                    118,
+                    32,
+                    87,
+                    260,
+                    99,
+                    104
+                }
+            };
+            yield return new object[] {
+                "Peter Piper picked a peck of pickled peppers. How many pickled peppers did Peter Piper pick?",
+                new List<int>
+                {
+                    80,
+                    101,
+                    116,
+                    101,
+                    114,
+                    32,
+                    80,
+                    105,
+                    112,
+                    259,
+                    32,
+                    112,
+                    105,
+                    99,
+                    107,
+                    101,
+                    100,
+                    32,
+                    97,
+                    266,
+                    101,
+                    269,
+                    32, 
+                    111,
+                    102,
+                    266,
+                    268,
+                    107,
+                    108,
+                    271,
+                    275,
+                    112,
+                    264,
+                    114,
+                    115,
+                    46,
+                    32,
+                    72,
+                    111,
+                    119,
+                    32,
+                    109,
+                    97,
+                    110,
+                    121,
+                    281,
+                    269,
+                    284,
+                    272,
+                    264,
+                    287,
+                    259,
+                    115,
+                    32,
+                    100,
+                    105,
+                    272,
+                    256,
+                    258,
+                    260,
+                    262,
+                    288,
+                    301,
+                    107,
+                    63
+                }
+            };
+        }
+    }
+}
---
+++ LZW/LZW/LZW/Compressor.cs
@@ -0,0 +1,50 @@
+﻿
+namespace LZW.LZW
+{
+    public class Compressor
+    {
+        private const short  Ascii_Length = 255;
+        public Dictionary<string, int> Dictionary { get; private set; }
+        public List<int> CompressedCode { get; private set; }
+
+        public Compressor()
+        {
+            Dictionary = new Dictionary<string, int>();
+            CompressedCode = new List<int>();
+            FillDictionary();
+        }
+
+        public List<int> Encode(string text)
+        {
+            string past = text[0].ToString(), current = string.Empty;
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                if (i != text.Length - 1)
+                {
+                    current += text[i + 1];
+                }
+                if (Dictionary.ContainsKey(past + current))
+                {
+                    past += current;
+                } else
+                {
+                    CompressedCode.Add(Dictionary[past]);
+                    int lastIndex = Dictionary.Last().Value;
+                    Dictionary.Add(past + current, lastIndex + 1);
+                    past = current;
+                }
+                current = string.Empty;
+            }
+            CompressedCode.Add(Dictionary[past]);
+            return CompressedCode;
+        }
+
+        private void FillDictionary()
+        {
+            for(int i = 0; i <= Ascii_Length; i++)
+            {
+                Dictionary.Add(((char)i).ToString(), i);
+            }
+        }
+    }
+}
